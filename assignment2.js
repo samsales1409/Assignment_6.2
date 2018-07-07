047ac81f06651bd50c4bc282d6a3c936cabba575
samsales1409 <38017063+samsales1409@users.noreply.github.com>
2018-07-07 17:37:31 +0530

    first commit

---
+++ assignment2.js
@@ -0,0 +1,22 @@
+//temprature console
+var recipeName ='Creamy squash linguine';
+var preparationTime=45;
+var recipeBy='Jimmy Cutter';
+var isVegetarian=true
+var calories=223.7
+console.log('RECIPIE DETAAILS');
+console.log('----------------------------');
+console.log(recipeName);
+console.log(preparationTime);
+console.log(recipeBy);
+console.log(isVegetarian);
+console.log(calories);
+//detail changed
+console.log('----------------------------');
+preparationTime=60;
+recipeBy='Jimmy Cutter George';
+console.log(recipeName);
+console.log(preparationTime);
+console.log(recipeBy);
+console.log(isVegetarian);
+console.log(calories);
